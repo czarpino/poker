08e57b8e8f91f5958f02a2b19b51ebc42ac820a1
Czar Pino <czarpino@gmail.com>
2014-01-09 15:32:48 +0800

    Added PokerUtility class

---
+++ app/lib/Foo/PokerOO/PokerUtility.php
@@ -0,0 +1,16 @@
+<?php namespace Foo\PokerOO;
+
+
+
+class PokerUtility
+{
+    public function createPlayers($numOflayers)
+    {
+        // TODO
+    }
+    
+    public function createDeck()
+    {
+        // TODO
+    }
+}
