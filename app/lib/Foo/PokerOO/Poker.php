24601c2d9473c3999c9e820afc7a26f22d06b6da
Czar Pino <czarpino@gmail.com>
2014-01-09 16:04:37 +0800

    Added mehthods to Poker

--- app/lib/Foo/PokerOO/Poker.php
+++ app/lib/Foo/PokerOO/Poker.php
@@ -4,9 +4,33 @@
 
 class Poker
 {
-    private $players;
     
-    private $deck;
+    private $players, $deck;
     
+    public function __construct($players, $deck)
+    {
+        $this->players = $players;
+        $this->deck = $deck;
+    }
+    
+    public function dealPrivateCards()
+    {
+        // TODO
+    }
+    
+    public function dealCommunityCards()
+    {
+        // TODO
+    }
+    
+    public function showdown()
+    {
+        // TODO
+    }
+    
+    public function getResults()
+    {
+        // TODO
+    }
     
 }
